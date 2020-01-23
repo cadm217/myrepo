a3661465daecb3168dbb7e39c7a0eee349ecf1f0
cadm217 <claire.de.march@duke.edu>
2020-01-23 14:08:37 -0500

    change color plot and size marks

--- src/plot_data.R
+++ src/plot_data.R
@@ -1,9 +1,9 @@
 # Plot the points out:
 ggplot(gmpts, aes(longitude, latitude, group = status))+
-  scale_fill_gradient(low = "grey70", high = "grey30", guide = "none") +
+  scale_fill_gradient(low = "coral2", high = "coral4", guide = "none") +
   xlab("Longitude") +
   ylab("Latitude") +
   theme_minimal()+
   facet_grid(~ status, labeller = label_value) +
   stat_density_2d(aes(fill = ..level..), geom = "polygon", color = "black", size = 0.2, alpha = 0.5) +
-  geom_point(color = "navy", size = .5, alpha = .4)
+  geom_point(color = "cyan1", size = .9, alpha = .4)
